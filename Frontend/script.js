fd3cc2d864b079e80f4506e3445ba3f795005b41
Chandan kumar Yedavalli <237r1a66c5@cmrtc.ac.in>
2026-06-12 10:20:56 +0530

    Version 1 - Basic Code Playground

---
+++ Frontend/script.js
@@ -0,0 +1,32 @@
+function runCode() {
+
+    let html = document.getElementById("htmlCode").value;
+    let css = document.getElementById("cssCode").value;
+    let js = document.getElementById("jsCode").value;
+
+    let output = document.getElementById("output");
+
+    output.srcdoc = `
+        <html>
+        <head>
+            <style>${css}</style>
+        </head>
+        <body>
+            ${html}
+
+            <script>
+                ${js}
+            <\/script>
+        </body>
+        </html>
+    `;
+}
+
+function clearCode() {
+
+    document.getElementById("htmlCode").value = "";
+    document.getElementById("cssCode").value = "";
+    document.getElementById("jsCode").value = "";
+
+    document.getElementById("output").srcdoc = "";
+}
