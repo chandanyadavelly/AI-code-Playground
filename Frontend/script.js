05bac1ad16012d1d149c611ff46706cd369a0a55
Chandan kumar Yedavalli <237r1a66c5@cmrtc.ac.in>
2026-06-12 12:03:37 +0530

    Version 2 - Monaco Editor Integration

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,32 +1,80 @@
+let htmlEditor;
+let cssEditor;
+let jsEditor;
+
+require.config({
+    paths: {
+        vs: "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.52.2/min/vs"
+    }
+});
+
+require(["vs/editor/editor.main"], function () {
+
+    htmlEditor = monaco.editor.create(
+        document.getElementById("htmlEditor"),
+        {
+            value: "<h1>Hello World</h1>",
+            language: "html",
+            theme: "vs-dark",
+            automaticLayout: true
+        }
+    );
+
+    cssEditor = monaco.editor.create(
+        document.getElementById("cssEditor"),
+        {
+            value: "h1 {\n  color: red;\n}",
+            language: "css",
+            theme: "vs-dark",
+            automaticLayout: true
+        }
+    );
+
+    jsEditor = monaco.editor.create(
+        document.getElementById("jsEditor"),
+        {
+            value: "console.log('Hello');",
+            language: "javascript",
+            theme: "vs-dark",
+            automaticLayout: true
+        }
+    );
+
+});
 function runCode() {
 
-    let html = document.getElementById("htmlCode").value;
-    let css = document.getElementById("cssCode").value;
-    let js = document.getElementById("jsCode").value;
+    let html = htmlEditor.getValue();
+    let css = cssEditor.getValue();
+    let js = jsEditor.getValue();
 
     let output = document.getElementById("output");
 
     output.srcdoc = `
-        <html>
-        <head>
-            <style>${css}</style>
-        </head>
-        <body>
-            ${html}
+    <html>
+    <head>
+        <style>
+        ${css}
+        </style>
+    </head>
 
-            <script>
-                ${js}
-            <\/script>
-        </body>
-        </html>
+    <body>
+
+        ${html}
+
+        <script>
+        ${js}
+        <\/script>
+
+    </body>
+    </html>
     `;
 }
 
 function clearCode() {
 
-    document.getElementById("htmlCode").value = "";
-    document.getElementById("cssCode").value = "";
-    document.getElementById("jsCode").value = "";
+    htmlEditor.setValue("");
+    cssEditor.setValue("");
+    jsEditor.setValue("");
 
     document.getElementById("output").srcdoc = "";
 }
